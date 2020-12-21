edda2570739b282cfd9e490507cf558019511b94
nganzalo <nganzalo@buffalo.edu>
2020-12-21 09:59:03 -0500

    Refactored template code & naming conventions of template code to be named apporpiatley for my plugin

--- src/main/java/com/TwitchEmotes/TwitchEmotesPlugin.java
+++ src/main/java/com/TwitchEmotes/TwitchEmotesPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.TwitchEmotes;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class TwitchEmotesPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private TwitchEmotesConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	TwitchEmotesConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(TwitchEmotesConfig.class);
 	}
 }
